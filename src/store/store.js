bb1039331014f79d51654f310accd277a15d549f
Martin V <martinvillarreal90@gmail.com>
2022-04-07 11:15:17 -0300

    modified logger and added redux-devtools extension"

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-//import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-//how logger works
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
-
-//const middleWares = [logger];
-const middleWares = [loggerMiddleware];
-
-const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 const persistConfig = {
   key: "root", //from root level it will store everything in the store
@@ -32,6 +12,21 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//Only run logger middleware when not in production
+// middleWares can't be null: filer(Boolean) will remove anything that is false and return an empty object
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+//using google chrome extension: Redux-DevTools
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
